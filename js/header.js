5fb71feb32b6f397d1c0f2731a4a0c122de85b2f
nekopaparo <taiwan799136579@gmail.com>
2021-09-17 21:01:28 +0800

    signout

--- js/header.js
+++ js/header.js
@@ -3,7 +3,7 @@ var headerMenu = document.getElementsByClassName("header-menu-isShow");
 function userState(){
     //如果是登入狀態，將登入改成登出
     if(myCookie.get("userState")){
-        headerMenu[0].innerHTML = '<a href="Homepage.html" onclick=delCookie("userState")>登出</a>';
+        headerMenu[0].innerHTML = '<a href="Homepage.html" onclick=myCookie.remove("userState")>登出</a>';
     }
     //非登入狀態，隱藏登入以外的連結
     else{
