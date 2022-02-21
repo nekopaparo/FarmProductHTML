08bce83f70e22a0b54ae407c8e4584887c5bc5ac
nekopaparo <89907578+nekopaparo@users.noreply.github.com>
2022-02-21 20:15:34 +0800

    資料讀取改成.json

--- js/SliderImg.js
+++ js/SliderImg.js
@@ -4,16 +4,22 @@ var recommend = `
     <img id="sliderImg" onclick="imgForward()">
 </div>
 <!--按鈕-->
-<div id="sliderRadio">
-    <script>
-        for(var i=0; i<imgPath.length; i++){
-            document.write('<div><input name="sliderRadio" type="radio" onclick="radioSelect()" value="'+ i +'"></div>');
-        }
-    </script>
-</div>
+<div id="sliderRadio"></div>
+<template  id="slider-Radio">
+    <div>
+        <input name="sliderRadio" type="radio" onclick="radioSelect()" value="">
+    </div>
+</template>
 <hr>
-<script>slideShow()</script>
+<script>
+    request.onload = function(){
+        data = request.response['data'];
+        addRadio();
+        slideShow();
+    };
+</script>
 `
+var data = null;
 //幻燈片效果用
 var images = 0;
 //圖片顯示時間 = slideSleep - imgSleep;
@@ -24,13 +30,13 @@ function slideShow(){
     //隱藏圖片
     document.getElementById("sliderImg").className = "fadeOut";
     t1 = setTimeout(function(){
-            document.getElementById("sliderImg").src = imgPath[images];
+            document.getElementById("sliderImg").src = `images/product/${data[images].number}01.jpg`;
             document.getElementById("sliderImg").className = "";       
             document.getElementsByName("sliderRadio")[images].checked = true; 
             images++;
         },imgSleep);
     //超過圖片範圍，從頭開始跑
-    if (images == imgPath.length) { images = 0; }
+    if (images == data.length) { images = 0; }
     t2 = setTimeout(slideShow, slideSleep);
 }
 //當有人按下radio按鍵時
@@ -42,4 +48,32 @@ function radioSelect(){
     clearTimeout(t2);
     //重新幻燈片
     slideShow();
+}
+//
+function addRadio(){
+    // 透過檢查 HTML template 元素屬性的存在與否，以測試瀏覽器是否支援它
+    if ('content' in document.createElement('template')) {
+        var tmp = document.querySelector('#slider-Radio');
+        var tag = tmp.content.querySelector("input");
+        var addpath = document.querySelector("#sliderRadio");
+        for(var i=0; i<data.length; i++){
+            tag.value = i;
+            // 複製新的radio並將其新增至sliderRadio
+            var clone = document.importNode(tmp.content, true);
+            addpath.appendChild(clone);
+        }
+    } else {
+        // 因為 HTML template 不被支援，所以要用其他方法在表格增加新行
+    }
+}
+//跳轉網頁
+function imgForward(){
+    var lastChange;
+    if(images == 0){
+        lastChange = data.length - 1;
+    }
+    else{
+        lastChange = images - 1;
+    }
+    SELECT_all_FROM_Commodity_WHERE_Commodity_id_productID(data[lastChange].number); //MySQL
 }
